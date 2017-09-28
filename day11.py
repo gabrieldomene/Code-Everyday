001345284b31c5cb6d03333bcd0831d55b44983a
Gabriel Domene <domenee.g@gmail.com>
2017-09-28 16:44:54 -0300

    Adjust in code

--- day11.py
+++ day11.py
@@ -1,11 +1,15 @@
 saltos = []
 media = 0
 while(True):
-	name = input("Nome do atleta: ")	
-	saltos = (int, input("Digite os valores das notas: ").split())
-	for i in range(5):
-		media = int(saltos[i]) + media
-	media = media/5
-	print(media)
+	name = input("Nome do atleta: ")
+	if(name == ""):
+		break
+	else:	
+		for i in range(5):
+			nota = int(input("Nota {0}: " .format(i+1)))
+			saltos.append(nota)
+		x = sum(saltos)/5
+	print("Média {0} = {1} pontos.".format(name, x))
+
 
 
